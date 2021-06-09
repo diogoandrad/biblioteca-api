e441ffeed4b4cfb3b7d2cfb7cb9740b6bee86ca8
diogoandrad <diogo10-1@hotmail.com>
2021-06-08 21:50:02 -0300

    Ajustado DTO

--- BIBLIOTECA.Domain/Dtos/BookDto.cs
+++ BIBLIOTECA.Domain/Dtos/BookDto.cs
@@ -4,7 +4,12 @@ namespace BIBLIOTECA.Domain.Dtos
 {
     public class BookDto
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string Title { get; set; }
+        public string Caption { get; set; }
+        public int Pages { get; set; }
+        public int Year { get; set; }
+        public double Version { get; set; }
+        public string Authors { get; set; }
     }
 }
